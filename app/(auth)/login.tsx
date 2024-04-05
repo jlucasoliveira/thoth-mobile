5b11fe35dd01b63ed70dec7ea6c8e51b287245bd
jlucasoliveirat <jose.lucas@teclat.com.br>
2024-04-05 13:49:54 -0300

    feat: add simple login screen to test

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Login(): React.JSX.Element {
+  return (
+    <View>
+      <Text style={{ fontFamily: "InterRegular" }}>Login</Text>
+    </View>
+  );
+}
